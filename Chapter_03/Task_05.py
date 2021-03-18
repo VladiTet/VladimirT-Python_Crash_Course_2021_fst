c55ba8161c0735792990e918682fc18fda49b750
VladiTet <vtet03@gmail.com>
2021-03-18 23:19:59 +0200

    fixed missing part from exercise task 05 #5

--- Chapter_03/Task_05.py
+++ Chapter_03/Task_05.py
@@ -3,6 +3,9 @@ guests = ['❤Nikol❤', 'Viktoria', 'Monika', 'Kristina']
 for x in guests:
     print(f"{x}, please come to dinner.")
 
+name = guests[1].title()
+print(f"\nSorry, {name} can't come to the dinner.\n")
+
 del(guests[1])
 guests.insert(1, 'Magi')
 
