8f9d5c8c81137b59698e6c762bf62f55f1bd81f0
VladiTet <vtet03@gmail.com>
2021-03-18 23:21:35 +0200

    add working task 06 #6

---
+++ Chapter_03/Task_06.py
@@ -0,0 +1,21 @@
+guests = ['❤Nikol❤', 'Viktoria', 'Monika', 'Kristina']
+
+for x in guests:
+    print(f"{x}, please come to dinner.")
+
+name = guests[1].title()
+print(f"\nSorry, {name} can't come to the dinner.\n")
+
+del(guests[1])
+guests.insert(1, 'Magi')
+
+for x in guests:
+    print(f"{x}, please come to dinner.")
+
+print("\nWe got a bigger table!\n")
+guests.insert(0, 'Bobi')
+guests.insert(2, 'Tisho')
+guests.append('Aleksander')
+
+for x in guests:
+    print(f"{x}, please come to dinner.")
