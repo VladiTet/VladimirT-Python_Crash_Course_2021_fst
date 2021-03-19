c5bb11336786b88670ebe5a7a8f981ee9b97783d
VladiTet <vtet03@gmail.com>
2021-03-19 14:56:11 +0200

    add working task 01 #12

---
+++ Chapter_04/Task_01.py
@@ -0,0 +1,9 @@
+favorite_pizzas = ['pepperoni','kaprichoza','kalcone']
+
+for x in favorite_pizzas:
+    print(x)
+
+for x in favorite_pizzas:
+    print(f"\nI really love {x} pizza")
+
+print("\nI really love pizza!")
