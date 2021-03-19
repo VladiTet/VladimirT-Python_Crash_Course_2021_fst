8644522ed0d93b69b19cf1bb1addab99e5108583
VladiTet <vtet03@gmail.com>
2021-03-19 15:10:09 +0200

    add working task 02 #13

---
+++ Chapter_04/Task_02.py
@@ -0,0 +1,9 @@
+favorite_pizzas = ['dog','lion','elephant']
+
+for x in favorite_pizzas:
+    print(x)
+
+for x in favorite_pizzas:
+    print(f"\nA {x} would make a great pet.")
+
+print("\nAny of these animals would make a great pet!")
