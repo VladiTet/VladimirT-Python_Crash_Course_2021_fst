c231176c1e93643c540ae3097878405462d64e20
VladiTet <vtet03@gmail.com>
2021-03-19 11:45:52 +0200

    add working task 10 #10

---
+++ Chapter_03/Task_10.py
@@ -0,0 +1,63 @@
+guests = ['❤Nikol❤', 'Viktoria', 'Monika', 'Kristina']
+
+for x in guests:
+    print(f"{x}, please come to dinner.")
+print(len(guests))
+
+name = guests[1].title()
+print(f"\nSorry, {name} can't come to the dinner.\n")
+
+del(guests[1])
+guests.insert(1, 'Magi')
+
+for x in guests:
+    print(f"{x}, please come to dinner.")
+
+print("\nWe got a bigger table!\n")
+guests.insert(0, 'Bobi')
+guests.insert(2, 'Tisho')
+guests.append('Aleksander')
+
+for x in guests:
+    print(f"{x}, please come to dinner.")
+
+print("Original order:")
+print(guests)
+
+print("\nAlphabetical:")
+print(sorted(guests))
+
+print("\nReverse alphabetical:")
+print(sorted(guests, reverse=True))
+
+print("\nReversed:")
+guests.reverse()
+print(guests)
+
+print("\nAlphabetical")
+guests.sort()
+print(guests)
+
+print("\nReverse alphabetical")
+guests.sort(reverse=True)
+print(guests)
+
+print(len(guests))
+
+print("\nSorry, we can only invite two people to dinner.\n")
+
+for x in range(0,5):
+    name = guests.pop()
+    print(f"Sorry, {name.title()} there's no room at the table.")
+
+print('\n')
+
+for x in guests:
+    print(f"{x}, please come to dinner.")
+
+print(len(guests))
+
+del(guests[0])
+del(guests[0])
+
+print(guests)
