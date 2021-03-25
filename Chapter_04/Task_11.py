eb1bedeed43acd93c780db9163b27ea722b1f7d8
VladiTet <vtet03@gmail.com>
2021-03-25 11:43:20 +0200

    add working task 11 #22

---
+++ Chapter_04/Task_11.py
@@ -0,0 +1,14 @@
+favorite_pizzas = ['pepperoni','kaprichoza','kalcone']
+friend_pizzas = favorite_pizzas[:]
+
+favorite_pizzas.append("meat lover's")
+friend_pizzas.append('pesto')
+
+print("My favorite pizzas are:")
+for x in favorite_pizzas:
+    print(f"-{x}")
+
+print("\nMy friend’s favorite pizzas are:")
+for x in friend_pizzas:
+    print(f"-{x}")
+
