1b511eefba344a7492e428cb0c04184f5534d263
VladiTet <vtet03@gmail.com>
2021-03-25 12:54:58 +0200

    add working task 13 #24

---
+++ Chapter_04/Task_13.py
@@ -0,0 +1,14 @@
+menu= ('sandwich', 'nuggets', 'burger', 'cakes', 'fries')
+
+print("You can choose from the following menu:")
+for x in menu:
+    print(f"-{x}")
+
+menu = ('rockfish sandwich', 'halibut nuggets', 'big mac burger',
+              'black cod tips', 'king crab legs')
+
+print("\nOur menu has been updated.")
+print("\nYou can now choose from the following menu:")
+
+for x in menu:
+    print(f"-{x}")
