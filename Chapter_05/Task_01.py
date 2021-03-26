c79af616c7f9b0c44fd64d61a95841742f77bd94
VladiTet <vtet03@gmail.com>
2021-03-26 10:55:52 +0200

    add working task 01 #25

---
+++ Chapter_05/Task_01.py
@@ -0,0 +1,40 @@
+car = 'subaru'
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+food = 'burger'
+
+print("\nIs food == 'burger'? I predict True.")
+print(food == 'burger')
+
+print("\nIs food == 'sandwich'? I predict False.")
+print(food == 'sandwich')
+
+vehicle = 'motorbike'
+
+print("\nIs vehicle == 'motorbike'? I predict True.")
+print(vehicle == 'motorbike')
+
+print("\nIs vehicle == 'bus'? I predict False.")
+print(vehicle == 'bus')
+
+annimal = 'lion'
+
+print("\nIs annimal == 'lion'? I predict True.")
+print(annimal == 'lion')
+
+print("\nIs annimal == 'dog'? I predict False.")
+print(annimal == 'dog')
+
+drinks = 'coke'
+
+print("\nIs drinks == 'coke'? I predict True.")
+print(drinks == 'coke')
+
+print("\nIs drinks == 'water'? I predict False.")
+print(drinks == 'water')
+
