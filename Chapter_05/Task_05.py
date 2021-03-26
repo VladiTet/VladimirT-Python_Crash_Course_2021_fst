9d1897e882c086ed1518824af9345e0fa520a1d7
VladiTet <vtet03@gmail.com>
2021-03-26 14:07:52 +0200

    fixes code style add working task 05 #29

--- Chapter_05/Task_05.py
+++ Chapter_05/Task_05.py
@@ -2,7 +2,11 @@ alien_color = 'red'
 
 if alien_color == 'green':
     print("You just earned 5 points")
+
 elif alien_color == 'yellow':
     print("You just earned 10 points")
+
 else:
     print("You just earned 15 points")
+
+
