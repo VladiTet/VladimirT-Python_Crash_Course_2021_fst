23be31291f2d9f17e3fc87c06c19fe1bb5d5333b
VladiTet <vtet03@gmail.com>
2021-03-26 14:17:16 +0200

    add working task 07 #31

---
+++ Chapter_05/Task_07.py
@@ -0,0 +1,17 @@
+favorite_fruits = ['blueberries','salmonberries','peaches']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+
+if 'peaches' in favorite_fruits:
+    print("You really like peaches!")
+
+if 'blueberries' in favorite_fruits:
+    print("You really like blueberries!")
+
