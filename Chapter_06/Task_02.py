b253375d2b0696fa6f83c78d55c9e60cf9b234f4
VladiTet <vtet03@gmail.com>
2021-04-01 11:54:34 +0300

    add working task 02 #37

---
+++ Chapter_06/Task_02.py
@@ -0,0 +1,22 @@
+favorite_numbers = {
+    'Maria': '8',
+    'Nikol': '26',
+    'George': '5',
+    'Aleksander': '12',
+    'Dimitar': '16',
+    }
+
+number = favorite_numbers['Maria']
+print(f"Maria's favorite number is {number}.")
+
+number = favorite_numbers['Nikol']
+print(f"Nikol's favorite number is {number}.")
+
+number = favorite_numbers['George']
+print(f"George's favorite number is {number}.")
+
+number = favorite_numbers['Aleksander']
+print(f"Aleksander's favorite number is {number}.")
+
+number = favorite_numbers['Dimitar']
+print(f"Dimitar's favorite number is {number}.")
