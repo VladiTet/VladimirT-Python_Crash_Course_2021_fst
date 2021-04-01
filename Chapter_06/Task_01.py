c3908009971234bbb795e67043cfee482f663de1
VladiTet <vtet03@gmail.com>
2021-04-01 11:39:38 +0300

    add working task 01 #36

---
+++ Chapter_06/Task_01.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'Vladi',
+    'last_name': 'Tetevenski',
+    'age': 17,
+    'city': 'Sofia',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
