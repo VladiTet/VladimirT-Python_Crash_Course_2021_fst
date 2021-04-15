db83042fd80eb97c1376770fe9780f6afbc927d2
Vladimir Tetevenski <vtet03@gmail.com>
2021-04-15 16:24:18 +0300

    add working task 13 #24 comment and uncomment you will have all of condition to the task fixing pep8 errors

--- Chapter_04/Task_13.py
+++ Chapter_04/Task_13.py
@@ -4,12 +4,12 @@ print("You can choose from the following menu:")
 for x in menu:
     print(f"-{x}")
 
-menu = ('rockfish sandwich', 'halibut nuggets', 'big mac burger',
-              'black cod tips', 'king crab legs')
+# menu[1] = 'chiken'
+
+menu = ('pizza', 'fish', 'burger', 'cakes', 'fries')
 
 print("\nOur menu has been updated.")
 print("\nYou can now choose from the following menu:")
 
 for x in menu:
     print(f"-{x}")
-
