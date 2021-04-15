bfad85a4bd7c49d44e1ac470d8c90296009635b0
Vladimir Tetevenski <vtet03@gmail.com>
2021-04-15 15:35:04 +0300

    add working task 10 #21 fixing hard code

--- Chapter_04/Task_10.py
+++ Chapter_04/Task_10.py
@@ -1,11 +1,11 @@
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
+players = ['charles', 'martina', 'michael', 'florence', 'eli', 'martin', 'George']
 
 print("Here are the first three players on my team:")
 for player in players[:3]:
     print(player.title())
 
 print("\nHere are the three players from middle on my team:")
-for player in players[1:4]:
+for player in players[int(len(players)/2 - 1):int(len(players)/2) + 2]:
     print(player.title())
 
 print("\nHere are the last three players on my team:")
