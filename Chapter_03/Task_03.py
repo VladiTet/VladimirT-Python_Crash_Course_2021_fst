7385c3bd75fa09df57fc20a9958e7a67b1c81fe9
Vladimir Tetevenski <vtet03@gmail.com>
2021-04-15 11:34:25 +0300

    add working task 03 #3 fixing pep8 errors

--- Chapter_03/Task_03.py
+++ Chapter_03/Task_03.py
@@ -2,12 +2,12 @@ motorcycle_list = ['Honda', 'Kawasaki', 'Harley Davidson', 'Simson', 'Mz',
                    'Yamaha', 'Suzuki']
 first_message = "I would like to own %s motorcycle :)"
 
-cars_list = ['Mercedes-Benz', 'BMW', 'Audi', 'Porshe', 'Ford', 'Rolls Royce',
-                 'Ferrari']
+cars_list = ['Mercedes-Benz', 'BMW', 'Audi', 'Porshe',
+             'Ford', 'Rolls Royce', 'Ferrari']
 second_message = "I would like to own %s car :)"
 
 for x in motorcycle_list:
-     print(first_message %x)
+    print(first_message % x)
 
 for x in cars_list:
-    print(second_message %x)
+    print(second_message % x)
