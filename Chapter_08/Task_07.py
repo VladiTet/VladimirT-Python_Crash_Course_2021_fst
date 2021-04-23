3e501d847d8f838b965630fb392e99eb605e463a
Vladimir Tetevenski <vtet03@gmail.com>
2021-04-23 14:49:26 +0300

    add working task 07 #63 add tracks

--- Chapter_08/Task_07.py
+++ Chapter_08/Task_07.py
@@ -1,9 +1,11 @@
-def make_album(artist, title):
+def make_album(artist, title, tracks=0):
     """Build a dictionary containing information about an album."""
     album_dict = {
         'artist': artist.title(),
         'title': title.title(),
         }
+    if tracks:
+        album_dict['tracks'] = tracks
     return album_dict
 
 album = make_album('metallica', 'ride the lightning')
@@ -13,4 +15,7 @@ album = make_album('beethoven', 'ninth symphony')
 print(album)
 
 album = make_album('willie nelson', 'red-headed stranger')
+print(album)
+
+album = make_album('iron maiden', 'piece of mind', tracks=8)
 print(album)
