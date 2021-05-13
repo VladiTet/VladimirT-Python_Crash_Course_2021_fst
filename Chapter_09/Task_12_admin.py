b4b2079b513f6fb1ad493721c93009651fa68283
Vladimir Tetevenski <vtet03@gmail.com>
2021-05-13 16:24:53 +0300

    add working task 12 #83

---
+++ Chapter_09/Task_12_admin.py
@@ -0,0 +1,24 @@
+from user import User
+
+
+class Admin(User):
+
+    def __init__(self, first_name, last_name, username, email, location):
+        """Initialize the admin."""
+        super().__init__(first_name, last_name, username, email, location)
+
+        self.privileges = Privileges()
+
+
+class Privileges():
+
+    def __init__(self, privileges=[]):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print("\nPrivileges:")
+        if self.privileges:
+            for privilege in self.privileges:
+                print(f"- {privilege}")
+        else:
+            print("- This user has no privileges.")
