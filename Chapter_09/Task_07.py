4756728ef47be9c5f1947b1cdc09e647ec054111
Vladimir Tetevenski <vtet03@gmail.com>
2021-05-13 11:26:03 +0300

    add working task 07 #78

---
+++ Chapter_09/Task_07.py
@@ -0,0 +1,49 @@
+class User():
+
+    def __init__(self, first_name, last_name, username, email, location):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.username = username
+        self.email = email
+        self.location = location.title()
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print(f"\n{self.first_name} {self.last_name}")
+        print(f"  Username: {self.username}")
+        print(f"  Email: {self.email}")
+        print(f"  Location: {self.location}")
+
+    def greet_user(self):
+        print(f"\nWelcome back, {self.username}!")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+class Admin(User):
+
+    def __init__(self, first_name, last_name, username, email, location):
+        super().__init__(first_name, last_name, username, email, location)
+        self.privileges = []
+
+    def show_privileges(self):
+        """Display the privileges this administrator has."""
+        print("\nPrivileges:")
+        for privilege in self.privileges:
+            print(f"- {privilege}")
+
+
+vladi = Admin('Vladi', 'Tetevenski', 'vtet03', 'vtet@example.com', 'Bulgaria')
+vladi.describe_user()
+
+vladi.privileges = [
+    'can reset passwords',
+    'can moderate discussions',
+    'can suspend accounts',
+    ]
+
+vladi.show_privileges()
